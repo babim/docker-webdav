b85fbcd05351410c00357bd9497c2a4a63a287dd
Duc Anh Babim <ducanh.babim@yahoo.com>
2016-11-30 02:08:23 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,99 @@
 #!/bin/sh
 set -x
 
+# set env for ACDCLI
+export CONFIGPATH=${CONFIGPATH:-/config}
+export CLOUDPATH=${CLOUDPATH:-/share}
+if [ ! -d "$CONFIGPATH" ]; then mkdir -p $CONFIGPATH; fi
+if [ ! -d "$CLOUDPATH" ]; then mkdir -p $CLOUDPATH; fi
+
+# set ID docker run
+auid=${auid:-1000}
+agid=${agid:-$auid}
+auser=${auser:-user}
+
+# webdav
 # Force user and group because lighttpd runs as webdav
-USERNAME=webdav
-GROUP=webdav
+USERNAME=${auser}
+GROUP=${auser}
+WEBDAVPASS=${WEBDAVPASS:-webdav}
 
 # Only allow read access by default
 READWRITE=${READWRITE:=false}
 
 # Add user if it does not exist
 if ! id -u "${USERNAME}" >/dev/null 2>&1; then
-    addgroup -g ${USER_GID:=2222} ${GROUP}
-    adduser -G ${GROUP} -D -H -u ${USER_UID:=2222} ${USERNAME}
+	addgroup -g ${agid:=2222} ${GROUP}
+	adduser -G ${GROUP} -D -H -u ${auid:=2222} ${USERNAME}
 fi
 
-chown webdav /var/log/lighttpd
+chown $USERNAME /var/log/lighttpd
+
+# create config for webdav
+if [ ! -f "$CONFIGPATH/lighttpd.conf" ]; then
+# create
+cat <<EOF>> $CONFIGPATH/lighttpd.conf
+server.modules = (
+    "mod_access",
+    "mod_accesslog",
+    "mod_webdav",
+    "mod_auth"
+)
+include "/etc/lighttpd/mime-types.conf"
+server.username       = "$USERNAME"
+server.groupname      = "$GROUP"
+server.document-root  = "$CLOUDPATH"
+server.pid-file       = "/run/lighttpd.pid"
+server.follow-symlink = "enable"
+# No errorlog specification to keep the default (stderr) and make sure lighttpd
+# does not try closing/reopening. And redirect all access logs to a pipe. See
+# https://redmine.lighttpd.net/issues/2731 for details
+accesslog.filename    = "/tmp/lighttpd.log"
+#Omitting the following on purpose
+#server.errorlog       = "/dev/stderr"
+include "$CONFIGPATH/webdav.conf"
+EOF
+fi
+
+# create config for webdav
+if [ ! -f $CONFIGPATH/webdav.conf ]; then
+cat <<'EOF'>> $CONFIGPATH/webdav.conf
+$HTTP["remoteip"] !~ "WHITELIST" {
+  # Require authentication
+  $HTTP["host"] =~ "." {
+EOF
+cat <<EOF>> $CONFIGPATH/webdav.conf
+    server.document-root = "$CLOUDPATH"
+    webdav.activate = "enable"
+    webdav.is-readonly = "disable"
+    webdav.sqlite-db-name = "/locks/lighttpd.webdav_lock.db" 
+    auth.backend = "htpasswd"
+    auth.backend.htpasswd.userfile = "/cache/htpasswd"
+    auth.require = ( "" => ( "method" => "basic",
+                             "realm" => "webdav",
+                             "require" => "valid-user" ) )
+  }
+}
+EOF
+cat <<'EOF'>> $CONFIGPATH/webdav.conf
+else $HTTP["remoteip"] =~ "WHITELIST" {
+  # Whitelisted IP, do not require user authentication
+  $HTTP["host"] =~ "." {
+EOF
+cat <<EOF>> $CONFIGPATH/webdav.conf
+    server.document-root = "$CLOUDPATH"
+    webdav.activate = "enable"
+    webdav.is-readonly = "disable"
+    webdav.sqlite-db-name = "/locks/lighttpd.webdav_lock.db" 
+  }
+}
+EOF
+fi
+
+# set webdav password
+if [ ! -f $CONFIGPATH/htpasswd ]; then
+htpasswd -cb $CONFIGPATH/htpasswd $auser $WEBDAVPASS
+fi
 
 # Create directory to hold locks
 mkdir /locks
@@ -25,36 +104,37 @@ chown ${USERNAME}:${GROUP} /locks
 # mount from the host, so do this conditionally.
 OWNERSHIP=${OWNERSHIP:=false}
 if [ "$OWNERSHIP" == "true" ]; then
-    chown -R webdav /webdav
-    chgrp -R webdav /webdav
+    chown -R $USERNAME $CLOUDPATH
+    chgrp -R $USERNAME $CLOUDPATH
 fi
 
 # Setup whitelisting addresses. Adresses that are whitelisted will not need to
 # enter credentials to access the webdav storage.
 if [ -n "$WHITELIST" ]; then
-    sed -i "s/WHITELIST/${WHITELIST}/" /etc/lighttpd/webdav.conf
+    sed -i "s/WHITELIST/${WHITELIST}/" $CONFIGPATH/webdav.conf
 fi
 
 # Reflect the value of READWRITE into the lighttpd configuration
 # webdav.is-readonly. Do this at all times, no matters what was in the file (so
 # that THIS shell decides upon the R/W status and nothing else.)
 if [ "$READWRITE" == "true" ]; then
-    sed -i "s/is-readonly = \"\\w*\"/is-readonly = \"disable\"/" /etc/lighttpd/webdav.conf
+    sed -i "s/is-readonly = \"\\w*\"/is-readonly = \"disable\"/" $CONFIGPATH/webdav.conf
 else
-    sed -i "s/is-readonly = \"\\w*\"/is-readonly = \"enable\"/" /etc/lighttpd/webdav.conf
+    sed -i "s/is-readonly = \"\\w*\"/is-readonly = \"enable\"/" $CONFIGPATH/webdav.conf
 fi
 
-# Copy good default configuration files if we had none.
-if [ ! -f /config/htpasswd ]; then
-    cp /etc/lighttpd/htpasswd /config/htpasswd
-fi
-
-if [ ! -f /config/webdav.conf ]; then
-    cp /etc/lighttpd/webdav.conf /config/webdav.conf
-fi
-
-# Run in foreground, see: https://redmine.lighttpd.net/issues/2731
 mkfifo -m 600 /tmp/lighttpd.log
 cat <> /tmp/lighttpd.log 1>&2 &
-chown webdav /tmp/lighttpd.log
-lighttpd -D -f /etc/lighttpd/lighttpd.conf 2>&1
+chown $USERNAME /tmp/lighttpd.log
+
+# mount amazon cloud drive to CLOUD PATH
+if [[ "$auid" = "0" ]] || [[ "$aguid" == "0" ]]; then
+    acdcli s
+    acdcli mount -ao $CLOUDPATH
+else
+    chown -R $auid:$agid $CLOUDPATH
+    su -c 'acdcli s' $auser
+    su -c 'acdcli mount -ao $CLOUDPATH' $auser
+fi
+
+lighttpd -D -f $CONFIGPATH/lighttpd.conf 2>&1
